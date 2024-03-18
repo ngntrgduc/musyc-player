ad7b47455115e04e3cc8521aeb691b976a095db8
ngntrgduc <trungducnguyen1511@gmail.com>
2024-03-19 00:56:38 +0700

    Add playlists and all songs feature

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-import pathlib
 from flask import Flask, render_template
-from song import get_songs
+from song import get_songs, get_playlists
 
 app = Flask(__name__)
 
 songs = get_songs()
+# TODO: Add parameter: all=False, to load all songs in music folder,
+# if not, no need to load all songs in playlists
 
 @app.route("/")
-def main(songs=songs):
+def main():
     return render_template('index.html', songs=songs)
+
+@app.route("/all")
+def all():
+    songs = get_songs(all=True)
+    return render_template('index.html', songs=songs)
+
+@app.route("/playlist")
+def playlist():
+    playlists = get_playlists()
+    return render_template('playlist.html', playlists=playlists)
+    #  if playlist selected, call main(songs=get_songs(folder))
